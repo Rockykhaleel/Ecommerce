2e29006893a21a06dbbaec0ce02718edddc5b72f
Rockykhaleel <r3khaleelshariff@gmail.com>
2023-11-18 12:34:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Chategory from "./Pages/Chategory";
+import AllProducts from "./Pages/AllProducts";
+import "./App.css"
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Products"Products element={<Products />} />
+          <Route path="/Login" exact element={<Login />} />
+          <Route path="/Signup" exact element={<Signup />} />
+          <Route path="/Chategory/:id" element={<Chategory />} />
+          <Route path="/AllProducts" exact element={<AllProducts />} />
+          <Route path="/Cart" exact element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
